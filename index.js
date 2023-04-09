330c0f20d498406fd79ab4364f095e3e7beefcff
Bal Elsada Hasun <fouadhason@gmail.com>
2023-04-09 23:49:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1 = 60;
+let num2 = 2;
+let num3 = 9
+let num4 = 5
+let multiply = num1 + num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4
+
+let max = Math.max(10, 20, 15, 19)
